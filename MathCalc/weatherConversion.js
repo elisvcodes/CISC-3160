41a232931a3774184850fb56a6126325ca6154f8
Eli <69530035+elisvcodes@users.noreply.github.com>
2020-09-09 18:09:02 -0400

    Update weatherConversion.js

--- MathCalc/weatherConversion.js
+++ MathCalc/weatherConversion.js
@@ -1,1 +1,18 @@
 
+const weatherForTheWeek = [85,90,87,92,78,81,78];
+
+let FtoC = weatherForTheWeek.map((f) => {
+    return ((f - 32) * (5/9)).toFixed(0)
+})
+
+let CtoF = FtoC.map((c) => {
+  return parseInt((c*(9/5) +32));
+})
+
+console.log('Fahrenheit to Celsius')
+console.log(FtoC);
+
+// The reason why when we convert to Celsius we lose a point is because when I was doing Fahrenheit to Celsius I trimmed all of the decimal numbers.
+
+console.log('Celsius to Fahrenheit')
+console.log(CtoF);
